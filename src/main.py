d125c61e634ec6da14ad668ab002bc0dd628884d
Vazanoir <romain.gianolio@proton.me>
2024-10-11 13:05:57 +0200

    add maze and draw it's cells

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from window import Window
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    Maze(
+        margin,
+        margin,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win
+    )
+
     win.wait_for_close()
 
 
---
+++ src/maze.py
@@ -0,0 +1,55 @@
+from draw import Cell
+from time import sleep
+
+
+class Maze():
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+    ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for x in range(self._num_cols):
+            col = []
+            for y in range(self._num_rows):
+                col.append(Cell(self._win))
+            self._cells.append(col)
+
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+
+        cell = self._cells[i][j]
+        x1 = self._x1 + self._cell_size_x * i
+        y1 = self._y1 + self._cell_size_y * j
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        cell.draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+
+        self._win.redraw()
+        sleep(0.05)
